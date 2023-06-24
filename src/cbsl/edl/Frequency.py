1d4a7cb72cf86ec0131a534117110b94fe5740c6
nuuuwan <nuuuwan@gmail.com>
2023-06-24 15:32:33 +0530

    Added more Frequencies

--- src/cbsl/edl/Frequency.py
+++ src/cbsl/edl/Frequency.py
@@ -15,4 +15,16 @@ class Frequency:
         return str(self)
 
 
-FREQUENCY_LIST = [Frequency('Annual', 'A', '1990', '2023')]
+START_YEAR = str(1970)
+END_YEAR = str(2024)
+
+FREQUENCY_LIST = [
+    Frequency('Annual', 'A', START_YEAR,END_YEAR),
+    # Frequency('Census Year', 'C', START_YEAR, END_YEAR),
+    # Frequency('Academic Year', 'E', START_YEAR,END_YEAR),
+    # Frequency('Monthly', 'M', f'{START_YEAR}-01', f'{END_YEAR}-01'),
+    # Frequency('Daily', 'D', f'{START_YEAR}-01-01', f'{END_YEAR}-01-01'),
+    # Frequency('Half Yearly', 'H', '1970', '2024'),
+    # Frequency('Quarterly', 'Q', '1970', '2024'),
+    # Frequency('On-Availablity', 'O', '1970', '2024'),
+]
