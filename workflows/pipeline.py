90512c7362b6fb5ddc9dbff3dac3043eaf8c355a
nuuuwan <nuuuwan@gmail.com>
2023-06-24 14:06:28 +0530

    Catch exception

--- workflows/pipeline.py
+++ workflows/pipeline.py
@@ -1,12 +1,17 @@
 from cbsl.edl import (FREQUENCY_LIST, N_SUBJECTS, PageSearchCriteria,
                       PageSearchResult, PageSelectItems)
-
+from utils import Log
+log = Log(__name__)
 
 def main():
     for frequency in FREQUENCY_LIST:
         for i_subject in range(0, N_SUBJECTS):
             webpage = PageSearchCriteria(frequency, i_subject).run()
-            webpage = PageSelectItems(webpage).run()
+            try:
+                webpage = PageSelectItems(webpage).run()
+            except:
+                log.error(f'Failed to complete {frequency}/{i_subject}.')
+                continue
             webpage = PageSearchResult(webpage).run()
             webpage.close()
 
