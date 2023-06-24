8d92706497b5949efa161c2ce04368c662db179a
nuuuwan <nuuuwan@gmail.com>
2023-06-24 12:57:10 +0530

    N_SUBJECTS = 35

--- src/cbsl/edl/pipeline.py
+++ src/cbsl/edl/pipeline.py
@@ -3,11 +3,12 @@ from cbsl.edl.PageSearchCriteria import PageSearchCriteria
 from cbsl.edl.PageSearchResult import PageSearchResult
 from cbsl.edl.PageSelectItems import PageSelectItems
 
+N_SUBJECTS = 35
+
 
 def main():
-    N_SUBJECT = 35
     for frequency in FREQUENCY_LIST:
-        for i_subject in range(0, N_SUBJECT):
+        for i_subject in range(0, N_SUBJECTS):
             webpage = PageSearchCriteria(frequency, i_subject).run()
             webpage = PageSelectItems(webpage).run()
             webpage = PageSearchResult(webpage).run()
