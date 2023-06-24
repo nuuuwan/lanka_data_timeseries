1769e007b4f5791f01c08a8fc056e3cbd74335b2
nuuuwan <nuuuwan@gmail.com>
2023-06-24 12:39:55 +0530

    Added FREQUENCY_LIST

--- src/cbsl/edl/Frequency.py
+++ src/cbsl/edl/Frequency.py
@@ -2,12 +2,11 @@ from dataclasses import dataclass
 
 
 @dataclass
-class FrequencyBase:
+class Frequency:
     name: str
     value: str
     from_str: str
     to_str: str
 
 
-class Frequency:
-    ANNUAL = FrequencyBase('Annual', 'A', '1970', '2023')
+FREQUENCY_LIST = [Frequency('Annual', 'A', '1990', '2023')]
