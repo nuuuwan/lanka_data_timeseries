6a1cf47f04d44461724f469b739a39f4ade9241a
nuuuwan <nuuuwan@gmail.com>
2023-06-24 13:11:05 +0530

    init for edl

---
+++ src/cbsl/edl/__init__.py
@@ -0,0 +1,6 @@
+from cbsl.edl.Frequency import FREQUENCY_LIST
+from cbsl.edl.PageSearchCriteria import PageSearchCriteria
+from cbsl.edl.PageSearchResult import PageSearchResult
+from cbsl.edl.PageSelectItems import PageSelectItems
+
+N_SUBJECTS = 35
