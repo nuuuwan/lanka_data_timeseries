2c3a0f8ff2b6b0204acaac9f07ea066383e94259
nuuuwan <nuuuwan@gmail.com>
2023-06-24 13:10:53 +0530

    Moved pipeline to workflows

--- workflows/pipeline.py
+++ workflows/pipeline.py
@@ -1,9 +1,5 @@
-from cbsl.edl.Frequency import FREQUENCY_LIST
-from cbsl.edl.PageSearchCriteria import PageSearchCriteria
-from cbsl.edl.PageSearchResult import PageSearchResult
-from cbsl.edl.PageSelectItems import PageSelectItems
-
-N_SUBJECTS = 35
+from cbsl.edl import (FREQUENCY_LIST, N_SUBJECTS, PageSearchCriteria,
+                      PageSearchResult, PageSelectItems)
 
 
 def main():
