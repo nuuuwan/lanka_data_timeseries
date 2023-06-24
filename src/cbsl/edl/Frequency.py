fb727fa9bc95737ba6f5b3c4d05c319b0850f43c
nuuuwan <nuuuwan@gmail.com>
2023-06-24 14:13:19 +0530

    Added __str__ and __repr__

--- src/cbsl/edl/Frequency.py
+++ src/cbsl/edl/Frequency.py
@@ -8,5 +8,11 @@ class Frequency:
     from_str: str
     to_str: str
 
+    def __str__(self):
+        return f'Frequency({self.name})'
+
+    def __repr__(self):
+        return str(self)
+
 
 FREQUENCY_LIST = [Frequency('Annual', 'A', '1990', '2023')]
