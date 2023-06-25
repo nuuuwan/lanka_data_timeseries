cd3b860509f53e34b5b659e9793a4e2d7fc5c141
nuuuwan <nuuuwan@gmail.com>
2023-06-25 07:39:42 +0530

    Moved FREQUENCY_LIST to new file

---
+++ src/cbsl/edl/FREQUENCY_LIST.py
@@ -0,0 +1,71 @@
+from utils import Time, TimeFormat
+
+from cbsl.edl.Frequency import Frequency
+
+
+def get_next_year():
+    return int(TimeFormat('%Y').stringify(Time.now())) + 1
+
+
+END_YEAR = get_next_year()
+START_YEAR = END_YEAR - 60
+
+FREQUENCY_LIST = [
+    # Frequency(
+    #     'Annual',
+    #     'A',
+    #     {
+    #         'ContentPlaceHolder1_txtYearFrom': START_YEAR,
+    #         'ContentPlaceHolder1_txtYearTo': END_YEAR,
+    #     },
+    # ),
+    # Frequency(
+    #     'Census Year',
+    #     'C',
+    #     {
+    #         'ContentPlaceHolder1_txtCensusYearFrom': START_YEAR,
+    #         'ContentPlaceHolder1_txtCensusYearTo': END_YEAR,
+    #     },
+    # ),
+    # Frequency(
+    #     'Academic Year',
+    #     'E',
+    #     {
+    #         'ContentPlaceHolder1_txtAcedemicYearFrom': START_YEAR,
+    #         'ContentPlaceHolder1_txtAcedemicYearTo': END_YEAR,
+    #     },
+    # ),
+    # Frequency(
+    #     'Half Yearly',
+    #     'H',
+    #     {
+    #         'ContentPlaceHolder1_txtHalfFrom': START_YEAR,
+    #         'ContentPlaceHolder1_txtHalfTo': END_YEAR,
+    #     },
+    # ),
+    # Frequency(
+    #     'Quarterly',
+    #     'Q',
+    #     {
+    #         'ContentPlaceHolder1_txtYearQuarterlyFrom': START_YEAR,
+    #         'ContentPlaceHolder1_txtYearQuarterlyTo': END_YEAR,
+    #     },
+    # ),
+    Frequency(
+        'Monthly',
+        'M',
+        {
+            'ContentPlaceHolder1_txtYearMonthly1': f'{START_YEAR}-01',
+            'ContentPlaceHolder1_txtYearMonthly2': f'{END_YEAR}-01',
+        },
+    ),
+    Frequency(
+        'Daily',
+        'D',
+        {
+            'ContentPlaceHolder1_txtDateFrom': f'{START_YEAR}-01-01',
+            'ContentPlaceHolder1_txtDateTo': f'{END_YEAR}-01-01',
+        },
+    ),
+    # Frequency('On-Availablity', 'O', {}),
+]
--- src/cbsl/edl/Frequency.py
+++ src/cbsl/edl/Frequency.py
@@ -1,36 +1,14 @@
 from dataclasses import dataclass
 
-from utils import Time, TimeFormat
-
-
-def get_next_year():
-    return int(TimeFormat('%Y').stringify(Time.now())) + 1
-
 
 @dataclass
 class Frequency:
     name: str
     value: str
-    from_str: str
-    to_str: str
+    input_text_map: dict[str, str]
 
     def __str__(self):
         return f'Frequency({self.name})'
 
     def __repr__(self):
         return str(self)
-
-
-END_YEAR = get_next_year()
-START_YEAR = END_YEAR - 60
-
-FREQUENCY_LIST = [
-    Frequency('Annual', 'A', START_YEAR, END_YEAR),
-    # Frequency('Monthly', 'M', f'{START_YEAR}-01', f'{END_YEAR}-01'),
-    # Frequency('Census Year', 'C', START_YEAR, END_YEAR),
-    # Frequency('Academic Year', 'E', START_YEAR,END_YEAR),
-    # Frequency('Daily', 'D', f'{START_YEAR}-01-01', f'{END_YEAR}-01-01'),
-    # Frequency('Half Yearly', 'H', '1970', '2024'),
-    # Frequency('Quarterly', 'Q', '1970', '2024'),
-    # Frequency('On-Availablity', 'O', '1970', '2024'),
-]
