e7b864ac9c1e20b00fc1fd5353cf8d1c017c0fa8
nuuuwan <nuuuwan@gmail.com>
2023-06-28 20:50:36 +0530

    FIXED from cbsl.edl.EDLCategoryTables import EDLCategoryTables  BUG

--- src/cbsl/edl/__init__.py
+++ src/cbsl/edl/__init__.py
@@ -1,5 +1,4 @@
 from cbsl.edl.Config import Config
-from cbsl.edl.EDLCategoryTables import EDLCategoryTables
 from cbsl.edl.EDLSummary import EDLSummary
 from cbsl.edl.FREQUENCY_LIST import FREQUENCY_LIST
 from cbsl.edl.PageSearchCriteria import PageSearchCriteria
