eabe53a7fb542b3efcb35627945395d101eb2774
nuuuwan <nuuuwan@gmail.com>
2023-07-03 11:14:47 +0530

    Added summary to pipelines

--- workflows/build_other_adb.py
+++ workflows/build_other_adb.py
@@ -1,4 +1,6 @@
 from lanka_data_timeseries.other_sources.adb import BuildData
+from lanka_data_timeseries.other_sources.adb import BuildSummary
 
 if __name__ == '__main__':
     BuildData.build_data()
+    BuildSummary.build()
--- workflows/build_other_misc.py
+++ workflows/build_other_misc.py
@@ -2,7 +2,9 @@ from lanka_data_timeseries.other_sources.misc.multi_data import \
     BuildData as BuildDataMulti
 from lanka_data_timeseries.other_sources.misc.single_data import \
     BuildData as BuildDataSingle
+from lanka_data_timeseries.other_sources.adb import BuildSummary
 
 if __name__ == '__main__':
     BuildDataMulti.build_data()
     BuildDataSingle.build_data()
+    BuildSummary.build()
