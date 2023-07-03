f23b803848e0e7ec9fc08871cb437cf077ae4494
nuuuwan <nuuuwan@gmail.com>
2023-07-03 11:29:07 +0530

    FIXED BuildSummary BUG

--- workflows/build_other_imf.py
+++ workflows/build_other_imf.py
@@ -1,5 +1,5 @@
 from lanka_data_timeseries.other_sources.imf import BuildData
-from lanka_data_timeseries.other_sources.adb import BuildSummary
+from lanka_data_timeseries.BuildSummary import BuildSummary
 
 if __name__ == '__main__':
     BuildData.build_data()
--- workflows/build_other_misc.py
+++ workflows/build_other_misc.py
@@ -2,7 +2,7 @@ from lanka_data_timeseries.other_sources.misc.multi_data import \
     BuildData as BuildDataMulti
 from lanka_data_timeseries.other_sources.misc.single_data import \
     BuildData as BuildDataSingle
-from lanka_data_timeseries.other_sources.adb import BuildSummary
+from lanka_data_timeseries.BuildSummary import BuildSummary
 
 if __name__ == '__main__':
     BuildDataMulti.build_data()
