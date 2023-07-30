6e54d0e4017572b10b34b2bc530970bf80422236
nuuuwan <nuuuwan@gmail.com>
2023-07-30 19:56:05 +0530

    Removed unused

--- workflows/build_other_world_bank.py
+++
@@ -1,6 +0,0 @@
-from lanka_data_timeseries.BuildSummary import BuildSummary
-from lanka_data_timeseries.other_sources.world_bank import BuildData
-
-if __name__ == '__main__':
-    BuildData.build_data()
-    BuildSummary.build()
