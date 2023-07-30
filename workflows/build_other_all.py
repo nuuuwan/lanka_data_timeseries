12aff2c185fe4462971c596cd5b1054fd8403373
nuuuwan <nuuuwan@gmail.com>
2023-07-30 19:44:06 +0530

    FIXED build_other_all BUG

--- workflows/build_other_all.py
+++ workflows/build_other_all.py
@@ -1,7 +1,20 @@
 from lanka_data_timeseries.BuildSummary import BuildSummary
-from lanka_data_timeseries.other_sources import adb, imf, misc, world_bank
+from lanka_data_timeseries.other_sources.adb import BuildData as BuildDataADB
+from lanka_data_timeseries.other_sources.imf import BuildData as BuildDataIMF
+from lanka_data_timeseries.other_sources.misc.multi_data import \
+    BuildData as BuildDataMiscMulti
+from lanka_data_timeseries.other_sources.misc.single_data import \
+    BuildData as BuildDataMiscSingle
+from lanka_data_timeseries.other_sources.world_bank import \
+    BuildData as BuildDataWorldBank
 
 if __name__ == '__main__':
-    for module in [adb, imf, world_bank, misc]:
-        module.BuildData.build_data()
+    for BuildData in [
+        BuildDataADB,
+        BuildDataIMF,
+        BuildDataWorldBank,
+        BuildDataMiscMulti,
+        BuildDataMiscSingle,
+    ]:
+        BuildData.build_data()
     BuildSummary.build()
