9330fb6a19c52d1801a20c594c673882b272840d
nuuuwan <nuuuwan@gmail.com>
2023-07-30 19:56:31 +0530

    Removed summary from build_other_all

--- workflows/build_other_all.py
+++ workflows/build_other_all.py
@@ -1,4 +1,3 @@
-from lanka_data_timeseries.BuildSummary import BuildSummary
 from lanka_data_timeseries.other_sources.adb import BuildData as BuildDataADB
 from lanka_data_timeseries.other_sources.imf import BuildData as BuildDataIMF
 from lanka_data_timeseries.other_sources.misc.multi_data import \
@@ -17,4 +16,3 @@ if __name__ == '__main__':
         BuildDataIMF,
     ]:
         BuildData.build_data()
-    BuildSummary.build()
