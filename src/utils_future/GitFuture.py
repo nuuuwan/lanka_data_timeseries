afd709557b05875b05d3717796d3953d80584e8c
nuuuwan <nuuuwan@gmail.com>
2023-07-30 12:44:05 +0530

    Updated get_last_update_ut

--- src/utils_future/GitFuture.py
+++ src/utils_future/GitFuture.py
@@ -1,12 +1,15 @@
+import time
+
 import git
+from utils import Time
 
 
 class GitFuture:
     def __init__(self, repo_path: str):
         self.repo = git.Repo(repo_path)
 
-    def get_last_update_time(self, file_path: str):
-        index_entry = self.repo.index[file_path]
-        commit = index_entry.commit
-        last_update_time = commit.authored_datetime
-        return last_update_time
+    def get_last_update_ut(self, file_path: str) -> Time:
+        commit = self.repo.iter_commits(
+            paths=file_path, max_count=1
+        ).__next__()
+        return Time(time.mktime(commit.authored_datetime.timetuple()))
