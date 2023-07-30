4cf2d65dd144d05fdeb01775872e5f8b1cc36b7e
nuuuwan <nuuuwan@gmail.com>
2023-07-30 11:58:04 +0530

    lint only

--- workflows/build_other_misc.py
+++ workflows/build_other_misc.py
@@ -1,8 +1,8 @@
+from lanka_data_timeseries.BuildSummary import BuildSummary
 from lanka_data_timeseries.other_sources.misc.multi_data import \
     BuildData as BuildDataMulti
 from lanka_data_timeseries.other_sources.misc.single_data import \
     BuildData as BuildDataSingle
-from lanka_data_timeseries.BuildSummary import BuildSummary
 
 if __name__ == '__main__':
     BuildDataMulti.build_data()
--- workflows/build_other_world_bank.py
+++ workflows/build_other_world_bank.py
@@ -1,5 +1,5 @@
-from lanka_data_timeseries.other_sources.world_bank import BuildData
 from lanka_data_timeseries.BuildSummary import BuildSummary
+from lanka_data_timeseries.other_sources.world_bank import BuildData
 
 if __name__ == '__main__':
     BuildData.build_data()
