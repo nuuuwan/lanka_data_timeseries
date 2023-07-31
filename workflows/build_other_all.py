a3743e7fa947abebc9161b2bb7c384afdf499506
nuuuwan <nuuuwan@gmail.com>
2023-07-31 16:19:06 +0530

    Added logging

--- workflows/build_other_all.py
+++ workflows/build_other_all.py
@@ -1,3 +1,5 @@
+from utils import Log
+
 from lanka_data_timeseries.other_sources.adb import BuildData as BuildDataADB
 from lanka_data_timeseries.other_sources.imf import BuildData as BuildDataIMF
 from lanka_data_timeseries.other_sources.misc.multi_data import \
@@ -7,6 +9,7 @@ from lanka_data_timeseries.other_sources.misc.single_data import \
 from lanka_data_timeseries.other_sources.world_bank import \
     BuildData as BuildDataWorldBank
 
+log = Log(__file__)
 if __name__ == '__main__':
     for BuildData in [
         BuildDataMiscMulti,
@@ -15,4 +18,5 @@ if __name__ == '__main__':
         BuildDataWorldBank,
         BuildDataIMF,
     ]:
+        log.info('Running ' + BuildData.__name__)
         BuildData.build_data()
