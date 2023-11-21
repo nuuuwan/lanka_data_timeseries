0e68ea6c0e3835e371913b4ea0cc52d58ae2bd60
nuuuwan <nuuuwan@gmail.com>
2023-11-21 09:13:43 +0800

    Removed unused

--- src/utils_future/__init__.py
+++ src/utils_future/__init__.py
@@ -2,7 +2,7 @@ from utils_future.FileFuture import FileFuture
 from utils_future.GitFuture import GitFuture
 from utils_future.Image import Image
 from utils_future.Point2D import Point2D
-from utils_future.Size2D import Size2D, Size2D16x9
+from utils_future.Size2D import Size2D
 from utils_future.Webpage import Webpage
 from utils_future.WebpageWrapper import WebpageWrapper
 from utils_future.WWWFuture import WWWFuture
