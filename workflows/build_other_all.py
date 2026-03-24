968e4d30bad576a7214b9d71a39be3e060e60e9f
nuuuwan <nuuuwan@gmail.com>
2026-03-24 09:03:06 +0530

    Refactor: Remove multi_data and single_data build scripts and associated data files
    - Deleted BuildData.py scripts for multi_data and single_data, along with their respective data files (CSV and JSON).
    - Removed helper functions related to data building for both multi_data and single_data.
    - Updated build_other_all.py to exclude multi_data and single_data from the build process.

--- workflows/build_other_all.py
+++ workflows/build_other_all.py
@@ -2,21 +2,16 @@ from utils import Log
 
 from lanka_data_timeseries.other_sources.adb import BuildData as BuildDataADB
 from lanka_data_timeseries.other_sources.imf import BuildData as BuildDataIMF
-from lanka_data_timeseries.other_sources.misc.multi_data import \
-    BuildData as BuildDataMiscMulti
-from lanka_data_timeseries.other_sources.misc.single_data import \
-    BuildData as BuildDataMiscSingle
-from lanka_data_timeseries.other_sources.world_bank import \
-    BuildData as BuildDataWorldBank
+from lanka_data_timeseries.other_sources.world_bank import (
+    BuildData as BuildDataWorldBank,
+)
 
 log = Log(__file__)
-if __name__ == '__main__':
+if __name__ == "__main__":
     for BuildData in [
-        BuildDataMiscMulti,
-        BuildDataMiscSingle,
         BuildDataADB,
         BuildDataWorldBank,
         BuildDataIMF,
     ]:
-        log.info('Running ' + BuildData.__name__)
+        log.info("Running " + BuildData.__name__)
         BuildData.build_data()
